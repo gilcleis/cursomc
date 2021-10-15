597082c279d7f511402cd95fc39791c5a1d60f99
Gilclei Araujo <gilclei@gmail.com>
2021-10-15 10:54:46 -0300

    Criado Repository e Service - Categoria

---
+++ src/main/java/com/gilclei/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.gilclei.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.OpAnd;
+import org.springframework.stereotype.Service;
+
+import com.gilclei.cursomc.domain.Categoria;
+import com.gilclei.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
