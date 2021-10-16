b2cd2b979a87ab1921faba8583b6e7f86b15ebe9
gil <gilclei@gmail.com>
2021-10-16 17:16:45 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/gilclei/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.gilclei.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilclei.cursomc.domain.Pedido;
+import com.gilclei.cursomc.repositories.PedidoRepository;
+import com.gilclei.cursomc.services.exeptions.ObjectNotFoundExeption;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExeption(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " 
+					+ Pedido.class.getSimpleName()));
+	}
+
+}
