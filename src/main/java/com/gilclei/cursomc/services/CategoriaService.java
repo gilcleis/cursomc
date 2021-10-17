230143009f9d1b8ce4ac3cfc83c186c64732953d
gil <gilclei@gmail.com>
2021-10-17 12:55:04 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/gilclei/cursomc/services/CategoriaService.java
+++ src/main/java/com/gilclei/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gilclei.cursomc.domain.Categoria;
@@ -14,17 +17,17 @@ import com.gilclei.cursomc.services.exeptions.ObjectNotFoundExeption;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExeption(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
-	public Categoria insert(Categoria obj) {		
+	public Categoria insert(Categoria obj) {
 		return repo.save(obj);
 	}
 
@@ -37,14 +40,18 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		}		
+		}
 	}
 
-	public List<Categoria> findAll() {	
-		return  repo.findAll();
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer perPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, perPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
