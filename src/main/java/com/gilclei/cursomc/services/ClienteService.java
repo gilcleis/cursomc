71a4e7caa2c80ebfe7252a5410321089c8930e55
gil <gilclei@gmail.com>
2021-10-17 20:26:04 -0300

    Não é possível excluir porque ha entidades relacionadas
    criado o metodo updateData() para controlar a atualização dos atributos permitidos

--- src/main/java/com/gilclei/cursomc/services/ClienteService.java
+++ src/main/java/com/gilclei/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.gilclei.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gilclei.cursomc.domain.Cliente;
+import com.gilclei.cursomc.dto.ClienteDTO;
 import com.gilclei.cursomc.repositories.ClienteRepository;
+import com.gilclei.cursomc.services.exeptions.DataIntegrityException;
 import com.gilclei.cursomc.services.exeptions.ObjectNotFoundExeption;
 
 @Service
@@ -19,6 +26,43 @@ public class ClienteService {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExeption(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		return repository.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente objNew = find(obj.getId());
+		updateData(objNew,obj);
+		return repository.save(objNew);
+	}
+
+	private void updateData(Cliente objNew, Cliente obj) {
+		objNew.setNome(obj.getNome());
+		objNew.setEmail(obj.getEmail());		
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque ha entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer perPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, perPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
 	}
 
 }
