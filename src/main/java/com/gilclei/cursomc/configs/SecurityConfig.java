76902f12f26fa289d2f4866f8979f81d4ae408ee
gil <gilclei@gmail.com>
2021-12-05 14:30:21 -0300

    Implementando autorização
     Criar um filtro de autorização. o Acrescentar uma função em JWTUtil para verificar se um dado token é válido.  Em SecurityConfig, registrar o filtro de autorização.

--- src/main/java/com/gilclei/cursomc/configs/SecurityConfig.java
+++ src/main/java/com/gilclei/cursomc/configs/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gilclei.cursomc.security.JWTAuthenticationFilter;
+import com.gilclei.cursomc.security.JWTAuthorizationFilter;
 import com.gilclei.cursomc.security.JWTUtil;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
